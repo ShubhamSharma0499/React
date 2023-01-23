4d275a28a936b438ebd83ba0406153481d7f603a
ShubhamSharma0499 <shubham.eng999@gmail.com>
2023-01-23 16:28:59 +0530

    made first functional component

---
+++ src/User.js
@@ -0,0 +1,10 @@
+function User(){  // functional component
+    return (
+        <>
+        <h1>User component</h1>
+        <h1>hello</h1> 
+        </> // in react we can only 
+    )
+}
+
+export default User; // only one component can be exported with default if you remove default then you will import user as {user}
