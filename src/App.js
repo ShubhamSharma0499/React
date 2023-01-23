deb9f47d1e22fceb9a5852d1a5e0d203ae1b4c29
ShubhamSharma0499 <shubham.eng999@gmail.com>
2023-01-23 16:37:25 +0530

    made first class component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import First from './First';
 import User from './User';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <User />
+        <First />
       </header>
     </div>
   );
---
+++ src/First.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class First extends React.Component
+{
+    render()
+    {
+        return (
+            <h1>first class component</h1>
+        )
+    }
+}
+
+export default First;
