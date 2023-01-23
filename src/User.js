d99e51cfc537887730bdf7d9f40552dd5a446888
ShubhamSharma0499 <shubham.eng999@gmail.com>
2023-01-23 17:26:00 +0530

    adding click event

--- src/User.js
+++ src/User.js
@@ -1,8 +1,12 @@
 function User(){  // functional component
+    function test(){
+        alert('test')
+    }
     return (
         <>
         <h1>User component</h1>
         <h1>hello</h1> 
+        <button onClick={test}>click</button> 
         </> // in react we can only 
     )
 }
