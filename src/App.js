c43244714f0f0f5ebd97d434118a2dd9e64c5362
ShubhamSharma0499 <shubham.eng999@gmail.com>
2023-01-23 16:38:51 +0530

    how to create component inside component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import First from './First';
 import User from './User';
 
 function App() {
+  function Test(){
+    return (<h1>test</h1>)
+  }
   return (
     <div className="App">
       <header className="App-header">
         <User />
         <First />
+        <Test />
       </header>
     </div>
   );
