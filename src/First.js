02616a8b4a958d3eb61c1ba2939ad1df1f0747b8
ShubhamSharma0499 <shubham.eng999@gmail.com>
2023-01-23 17:42:52 +0530

    adding state in class  component to change data and rerender component after making changes to data

--- src/First.js
+++ src/First.js
@@ -2,10 +2,24 @@ import React from 'react';
 
 class First extends React.Component
 {
+    constructor(){
+        super();
+        this.state={
+            data:0
+        }
+    }
+    change() {
+        this.setState({
+            data:this.state.data + 1
+        })
+    }
     render()
     {
         return (
-            <h1>first class component</h1>
+            <>
+            <h1>{this.state.data}</h1>
+            <button onClick={()=> this.change()}>change</button>
+            </>
         )
     }
 }
